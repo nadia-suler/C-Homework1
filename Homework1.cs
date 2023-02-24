7cfb6dde8073f981046302c27b14f2eb9cde3733
nadia-suler <103899890+nadia-suler@users.noreply.github.com>
2023-02-24 23:12:19 +0100

    Homework1.cs
    Homework 1

---
+++ Homework1.cs
@@ -0,0 +1,61 @@
+/*    
+    // Task 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+      Console.Write("Input the first number: ");
+      int numberA = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Input the second number: ");
+      int numberB = Convert.ToInt32(Console.ReadLine());
+
+      if(numberA > numberB)
+      {
+        Console.WriteLine($"max = {numberA}, min = {numberB}");
+      }
+      else
+      {
+        Console.WriteLine($"max = {numberB}, min = {numberA}");
+      }
+    */
+
+    /*
+    // Task 4.Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+      Console.Write("Input the first number: ");
+      int numberA = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Input the second number: ");
+      int numberB = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Input the third number: ");
+      int numberC = Convert.ToInt32(Console.ReadLine());
+
+      int max = numberA;
+      if(max < numberB) max = numberB;
+      if(max < numberC) max = numberC;
+
+      Console.WriteLine($"max = {max
+      */ 
+
+    /*
+    // Task 6.  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+      Console.Write("Input a number: ");
+      int num = Convert.ToInt32(Console.ReadLine());
+
+      if(num % 2 == 0) 
+      {
+        Console.WriteLine("Yes!");
+      }
+      else
+      {
+        Console.WriteLine("No :(");
+      }
+    */
+
+    /*
+    // Task 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+    Console.Write("Input a positive number: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    int current = 2;
+
+    while(current <= num)
+    {
+      Console.Write(current + " ");
+      current += 2;
+    }
+    */
